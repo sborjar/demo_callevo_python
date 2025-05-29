2a2ef6f4cbafdf365a97b67328462261ce8c41a1
Santiago Borja Romero <santiago@callevo.com>
2025-05-29 16:48:55 +0000

    Support timezones routes

---
+++ src/timezones/get-timezones-id.py
@@ -0,0 +1,27 @@
+import requests
+import sys
+import os
+sys.path.insert(0, os.getcwd())
+from utils.functions import saveMdFiles,getFileNameMethod
+current_file_name, current_file_method = getFileNameMethod(__file__)
+from dotenv import load_dotenv
+load_dotenv()
+
+url = f'{os.getenv("API_PATH")}timezones/1525'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {os.getenv("APP_TOKEN")}',
+}
+params = None
+response = requests.get(url, json=params, headers=headers)
+
+if response.status_code == 200:
+    print('Successful request')
+    print('Data:', response.json())
+    saveMdFiles(current_file_name,current_file_method,url,headers,params,response.json())
+else:
+    print('Error in the request, details:', response.text)
+    print('Details:')
+    print('Status Code:', response.status_code)
+    print(response)
